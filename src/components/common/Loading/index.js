e2dc589a3d6cc27445560cb1bc81837577af507a
Elizabeth Lofredo <e.lofredo@gmail.com>
2020-10-26 14:48:48 -0300

    improve design

---
+++ src/components/common/Loading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { number, string } from 'prop-types';
+import Loader from 'react-loader-spinner';
+
+import { primaryColor } from '../../../constants/common';
+
+const Loading = ({ height = 50, width = 50, color = primaryColor }) => (
+  <div className="loading">
+    <Loader type="Bars" color={color} height={height} width={width} />
+  </div>
+);
+
+Loading.propTypes = {
+  width: number,
+  height: number,
+  color: string,
+};
+
+export default Loading; 
---
+++ src/constants/common.js
@@ -0,0 +1,1 @@
+export const primaryColor = '#f24462';
